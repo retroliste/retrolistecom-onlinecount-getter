4c67dd817f248ed35a4c8d7f22129b68838471aa
niclas3332 <niclas@retroliste.com>
2022-06-19 03:00:55 +0200

    test

--- src/main/java/com/retroliste/plugin/main.java
+++ src/main/java/com/retroliste/plugin/main.java
@@ -10,20 +10,21 @@ import com.eu.habbo.plugin.events.emulator.EmulatorLoadedEvent;
 import com.eu.habbo.plugin.events.users.UserDisconnectEvent;
 import com.eu.habbo.plugin.events.users.UserLoginEvent;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 
-import com.eu.habbo.threading.runnables.AchievementUpdater;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+
+import java.net.URL;
 import org.apache.http.HttpHeaders;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
 public class main extends HabboPlugin implements EventListener {
-    private final Logger LOGGER = LoggerFactory.getLogger(Emulator.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Emulator.class);
 
 
     @Override
@@ -115,20 +116,12 @@ public class main extends HabboPlugin implements EventListener {
 
         Thread newThread = new Thread(() -> {
 
-            HttpClient client = HttpClient.newHttpClient();
 
             try {
-                HttpRequest request = HttpRequest.newBuilder(URI.create(apiEndpoint + hotelId))
-                        .header("content-type", "application/json")
-                        .setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + key)
-                        .setHeader(HttpHeaders.ACCEPT, "application/json")
-                        .POST(HttpRequest.BodyPublishers.ofString(e))
-                        .build();
-
-                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-            } catch (Exception ignored) {
-
+             String answer = executePost(apiEndpoint + hotelId, e, key);
+             LOGGER.info(answer);
+            } catch (Exception x) {
+                LOGGER.error(x.getMessage());
             }
 
         });
@@ -137,4 +130,51 @@ public class main extends HabboPlugin implements EventListener {
 
 
     }
+
+
+    public static String executePost(String targetURL, String urlParameters, String key) {
+        HttpURLConnection connection = null;
+
+        try {
+            //Create connection
+            URL url = new URL(targetURL);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type",
+                    "application/json");
+            connection.setRequestProperty(HttpHeaders.ACCEPT, "application/json");
+            connection.setRequestProperty(HttpHeaders.AUTHORIZATION, "Bearer " + key);
+            connection.setRequestProperty("Content-Length",
+                    Integer.toString(urlParameters.getBytes().length));
+
+            connection.setUseCaches(false);
+            connection.setDoOutput(true);
+
+            //Send request
+            DataOutputStream wr = new DataOutputStream (
+                    connection.getOutputStream());
+            wr.writeBytes(urlParameters);
+            wr.close();
+
+            //Get Response
+            InputStream is = connection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
+            String line;
+            while ((line = rd.readLine()) != null) {
+                response.append(line);
+                response.append('\r');
+            }
+            rd.close();
+            return response.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+    }
+
 }
