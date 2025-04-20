5505053e6f6b0f7544be04d9a22365dd58d89d63
Markus <niclas@nhabbo.com>
2025-04-20 18:27:58 +0200

    Removed unused Imports
    Signed-off-by: Markus <niclas@nhabbo.com>

--- src/main/java/com/retroliste/plugin/UpdateChecker.java
+++ src/main/java/com/retroliste/plugin/UpdateChecker.java
@@ -1,24 +1,18 @@
 package com.retroliste.plugin;
 
-import com.eu.habbo.Emulator;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
